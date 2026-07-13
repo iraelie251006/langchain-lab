2b50515e56fbd2befe69a48e49251dcf13cd454d
Elie <niyubwayoiraelie5777@gmail.com>
2026-07-13 23:53:23 +0200

    Add initial implementation of a roadmap creator assistant using langchain

---
+++ practice.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain.chat_models import init_chat_model
+from langchain.messages import HumanMessage
+
+load_dotenv()
+
+request = input("How can I help you today: ")
+
+llm = init_chat_model(model="openai:gpt-4o-mini", temperature=0.2)
+
+prompt = """
+You are Senior Staff engineer at Meta, 
+you are helpful roadmap creator assistant who helps people to break into Meta industry, 
+you don't provide generic roadmaps you focus on what really matters and what's need to be done in-order for engineer to break in,
+focusing on real work at scale mentality. 
+"""
+
+agent = create_agent(model=llm, tools=[], system_prompt=prompt)
+
+question = HumanMessage(content=request)
+
+for chunk in agent.stream({"messages": [question]}, stream_mode="messages"):
+    msg = chunk[0]
+    if hasattr(msg, "content"):
+        print(msg.content, end="", flush=True)
+
+
